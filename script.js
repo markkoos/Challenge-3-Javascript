6d1f171dbc4af63af567a9b7c170bf9bb38d79a0
markkoos <markusimercado@gmail.com>
2022-07-23 13:50:58 -0700

    I'M DONE

--- script.js
+++ script.js
@@ -6,26 +6,7 @@ var specialChars = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`,];
 var numChars = [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `0`,];
 var lowChars = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`,];
 var uppChars = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`,];
-var genPass 
-console.log(specialChars);
-// function for the promps and confirms
-function specialConfirm() {
-  window.confirm(`Do you want to include special characters?`);
-}
-
-function numConfirm() {
-  window.confirm(`Do you also want to include numerical characters?`);
-}
-
-function lowConfirm() {
-  window.confirm(`Do you also want to include lowercase characters?`);
-}
-
-function uppConfirm() {
-  window.confirm(`Do you also want to include uppercase characters?`);
-}
-
-
+var madePass = ``
 
 // Write password to the #password input
 function writePassword() {
@@ -44,22 +25,22 @@ function writePassword() {
       window.alert(`you need to specify how many characters you want; try again`)
       return; 
 
-    } specialConfirm(); {
+    } specialConfirm = window.confirm(`Do you want to include special characters?`); {
 
     } if (specialConfirm) {
         passArray = passArray.concat(specialChars);
 
-    } numConfirm(); {
+    } numConfirm = window.confirm(`Do you also want to include numerical characters?`); {
       
     } if (numConfirm) {
         passArray = passArray.concat(numChars);
 
-    } lowConfirm(); {
+    } lowConfirm = window.confirm(`Do you also want to include lowercase characters?`); {
     
     } if (lowConfirm) {
         passArray = passArray.concat(lowChars);
 
-    } uppConfirm(); {
+    } uppConfirm = window.confirm(`Do you also want to include uppercase characters?`); {
     
     } if (uppConfirm) {
         passArray = passArray.concat(uppChars);
@@ -68,31 +49,16 @@ function writePassword() {
     
     // for-loop that creates the password being generated
 
-      for (var i = 0; i < passwordLength; i++) {
+    for (var i = 0; i < passwordLength; i++) {
       var passRandom = Math.floor(Math.random() * passArray.length);
-      genPass += passArray.slice(passRandom, passRandom +1);
-      password = genPass
-    
-    
-  }
+      madePass += passArray.slice(passRandom, passRandom +1);
+      
+      
+    }
   
-  console.log(genPass);
-  console.log(passRandom);
-  console.log(password);
-  console.log(passwordLength);
-  console.log(passArray);
-  console.log(typeof passArray)
-  
-  
-  }
-
-    var password = generatePassword();
+  } var password = generatePassword();
     var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-    console.log(passwordText);
-
+    passwordText.value = madePass;
 
 }
 
